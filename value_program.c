9a7f47dd9ef8951aa60a168d5f16b38edc44aadd
nithishr2k <e22bm053@shanmugha.edu.in>
2025-05-06 16:35:21 +0530

    Create value_program.c

---
+++ value_program.c
@@ -0,0 +1,14 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int num = 50;
+    if (num >100){
+        printf("the given number is greater than the value.\n");
+       } else{
+            printf("the given number is less than the value.\n");
+       }
+    return 0;
+}
+        
+        
