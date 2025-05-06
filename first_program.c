c5df41effe895f90b11512fd4dfdac5751eca493
nithishr2k <e22bm053@shanmugha.edu.in>
2025-05-06 16:33:35 +0530

    Create first_program.c

---
+++ first_program.c
@@ -0,0 +1,10 @@
+/*
+ this program for arithmetic operatons ,and name:nithish, date:5.08.2025,c program
+ */
+ #include<stdio.h>
+     int main(){
+            printf("%f\n", 27.5+10);
+            printf("%f", 10.5+10);
+         return 0;
+     }
+ 
