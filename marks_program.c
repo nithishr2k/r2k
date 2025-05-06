048350bbaca407da664c9bf168ba33b86bd06b40
nithishr2k <e22bm053@shanmugha.edu.in>
2025-05-06 16:34:20 +0530

    Create marks_program.c

---
+++ marks_program.c
@@ -0,0 +1,18 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int marks = 50;
+    if (marks>=100){
+        printf("Grade: A\n");
+    } else if (marks>=80){
+        printf("Grade: B\n");
+    }else if (marks>=50){
+        printf("Grade: C\n");
+    }else {
+        printf("Grade: U\n");
+    }
+    return 0 ;
+}
+    
+    
