f32e4f96addcc2248cec86eb5354b1a4902b57aa
nithishr2k <e22bm053@shanmugha.edu.in>
2025-05-06 16:32:35 +0530

    Create addition_program.c

---
+++ addition_program.c
@@ -0,0 +1,15 @@
+/*
+ 
+ this program for arithmetic operatons ,and name:nithish, date:5.08.2025,c program
+ */
+ #include<stdio.h>
+     int main(){
+         int a, b;
+         int sum;
+            printf("Enter the two numbers:" );
+            scanf("%d %d",&a, &b);
+            sum=a+b;
+            printf("%d",sum);
+            return 0;
+     }
+ 
